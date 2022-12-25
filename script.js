b9403ecabd0b3b392814794455aba6ad33070bd9
Daniel Thauan <daniel_thauan@hotmail.com>
2022-12-25 19:24:29 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // if (html.classList.contains('light')) {
+  //   html.classList.remove('light')
+  // } else {
+  //   html.classList.add('light')
+  // }
+
+  // pegar a tag img
+  const img = document.querySelector('#profile img')
+
+  // substituir a imagem
+  if (html.classList.contains('light')) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light-mode.png')
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute('src', './assets/avatar.png')
+  }
+
+  if (html.classList.contains('light')) {
+    img.setAttribute(
+      'alt',
+      'Imagem do Daniel Thauan de mascara lendo um livro em uma livraria.'
+    )
+  } else {
+    img.setAttribute(
+      'alt',
+      'Foto de Daniel Thauan no supermercado usando boné e fone de ouvido.'
+    )
+  }
+}
